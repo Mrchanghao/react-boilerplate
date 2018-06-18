57045a2b1aca7d3b30c89dbf591d28a9ea4dabb4
이창호 <lnl2048@gmail.com>
2018-06-18 19:08:52 +0900

    react 상태 해지하기 과제

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/app';
+import App from './containers/app';
+import Home from './containers/Home';
 
 
-
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<Home membership='guest' />, document.getElementById('root'))
 
 
 
